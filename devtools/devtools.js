7c0f8c0f11aefe10bcd9b15b4cc595278fa32da3
Espen Henriksen <dev@henriksen.is>
2018-11-09 20:33:21 +0100

    Make it work in chrome.. Kinda

--- devtools/devtools.js
+++ devtools/devtools.js
@@ -27,60 +27,61 @@ const Row = ({ status, statusText, url }) => htmlToElement(`
   </tr>
 `);
 
-function panelHandler({ document }) {
-  const tbody = document.querySelector('tbody');
-  document.querySelector('#clear').addEventListener(
-    'click',
-    () => document.querySelector('tbody').innerHTML = '',
-  );
-
-  browser.devtools.network.onRequestFinished.addListener(request => {
-    const { url } = request.request;
-    const { status, statusText } = request.response;
-
-    // TODO a lot of responses come through with status 0 and missing data.
-    // Filter them out until we can figure out what is going on
-    if (!status) return;
-
-    const row = Row({ url, status, statusText });
-
-    row
-      .querySelector('.copy')
-      .addEventListener('click', async () => {
-        const [ body, contentType ] = await request.getContent();
-        browser.runtime.sendMessage({
-          type: 'copyText',
-          data: renderText({
-            ...request,
-            contentType,
-            body,
-          }),
-        });
-      })
-
-    row
-      .querySelector('.export')
-      .addEventListener('click', async () => {
-        const [ body, contentType ] = await request.getContent();
-        browser.runtime.sendMessage({
-          type: 'exportHtml',
-          data: renderHtml({
-            ...request,
-            contentType,
-            body,
-          }),
-        });
-      })
-
-    tbody.appendChild(row);
-  });
-};
-
 browser.devtools.panels.create(
   "Savior",
   "icon-512.png",
-  "panel/panel.html"
+  "/devtools/panel/panel.html"
 ).then(newPanel => {
+  function panelHandler({ document }) {
+    const tbody = document.querySelector('tbody');
+    document.querySelector('#clear').addEventListener(
+      'click',
+      () => document.querySelector('tbody').innerHTML = '',
+    );
+
+    browser.devtools.network.onRequestFinished.addListener(request => {
+      const { url } = request.request;
+      const { status, statusText } = request.response;
+
+      // TODO a lot of responses come through with status 0 and missing data.
+      // Filter them out until we can figure out what is going on
+      // Probably caused by https://bugzilla.mozilla.org/show_bug.cgi?id=1472653
+      if (!status) return;
+
+      const row = Row({ url, status, statusText });
+
+      row
+        .querySelector('.copy')
+        .addEventListener('click', async () => {
+          const [ body, contentType ] = await request.getContent();
+          browser.runtime.sendMessage({
+            type: 'copyText',
+            data: renderText({
+              ...request,
+              contentType,
+              body,
+            }),
+          });
+        })
+
+      row
+        .querySelector('.export')
+        .addEventListener('click', async () => {
+          const [ body, contentType ] = await request.getContent();
+          browser.runtime.sendMessage({
+            type: 'exportHtml',
+            data: renderHtml({
+              ...request,
+              contentType,
+              body,
+            }),
+          });
+        })
+
+      tbody.appendChild(row);
+    });
+  };
+
   newPanel.onShown.addListener(panelHandler);
   newPanel.onHidden.removeListener(panelHandler);
 });
